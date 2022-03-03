156781a83847ca7ab49641eb5a2e505c496f5bc2
muhammadanani <maanani96@gmail.com>
2022-03-03 10:36:54 +0100

    update

---
+++ config_tweepy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tweepy
+
+auth = tweepy.OAuth2BearerHandler("AAAAAAAAAAAAAAAAAAAAAEQCZwEAAAAA0sLRyTIrXnXhebDLC1YJM3qTqRk%3DEsUwVKUJ7Fzu5tyY0FmzwZkB9BNpEKRMoSGaTfXZjcuzBJm9Az")
+api = tweepy.API(auth)
+
+
+# In[ ]:
+
+
+
+
